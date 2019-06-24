108d635ff18b55a4d441c7583348690c58ef15f4
BALAJI POTHULA <balan.pothula@gmail.com>
2019-06-24 15:14:37 +0530

    Update setup-java8-redhat.sh

--- setup-java8-redhat.sh
+++ setup-java8-redhat.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+
+# Author      : BALAJI POTHULA <balaji.pothula@techie.com>,
+# Date        : 31 August 2016,
+# Description : Installing java on rhel, ubuntu.
+
+# installing java-1.8.0 on rhel.
 sudo yum -y install java-1.8.0-openjdk-devel
+
+# installing java-1.8.0 on ubuntu.
+sudo apt -y install openjdk-8-jdk
